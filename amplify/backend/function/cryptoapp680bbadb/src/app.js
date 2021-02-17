9f0c379c4c24617d555f4a47b8a3cfa675c1d97b
pugDad26 <63824751+pugDad26@users.noreply.github.com>
2021-02-16 18:20:11 -0600

    added /coins route
    did some editing...bugs
    
    reformated code

--- amplify/backend/function/cryptoapp680bbadb/src/app.js
+++ amplify/backend/function/cryptoapp680bbadb/src/app.js
@@ -29,6 +29,22 @@ app.use(function(req, res, next) {
 /**********************
  * Example get method *
  **********************/
+app.get(
+  '/coins'
+  , (req, res) => {
+    const coins = [
+      { name: 'Bitcoin', symbol: 'BTC', price_usd: "10000" },
+      { name: 'Ethereum', symbol: 'ETH', price_usd: "400" },
+      { name: 'Litecoin', symbol: 'LTC', price_usd: "150" }
+    ];
+    res.json({
+      coins
+
+      //above shorthand for coins: coins
+      
+    });
+  }
+);
 
 app.get('/item', function(req, res) {
   // Add your code here
