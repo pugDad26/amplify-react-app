efc76512dca50463ae760d81ca1f9e3ae60bad78
pugDad26 <63824751+pugDad26@users.noreply.github.com>
2021-02-23 23:59:51 -0600

    starting project3

--- src/GitHubBornOn.js
+++ src/GitHubBornOn.js
@@ -1,6 +1,26 @@
-import React from 'react';
+import { API } from 'aws-amplify';
+import React 
+    , {
+        useState,
+        useEffect
+    }
+from 'react';
 
-export const GitHubBornOn = () => {
+export const GitHubBornOn = async () => {
+
+    //Variable to hold github creation date
+    const [createdOn, showCreationDate] = useState("");
+
+    //Function that will call API get/born
+    const fetchGitHubCreationDate = () => {
+        try {
+            //Try to get the data from the API endpoint /born
+            const data = await API.get('', '/born');
+        }
+        catch(err) {
+            console.error(err);
+        }
+    }
     return(
         <h2>
             my github name goes here - my github created at goes here
