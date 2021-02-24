b5f4041ab43acdec668c719cf477749bc02555ac
pugDad26 <63824751+pugDad26@users.noreply.github.com>
2021-02-24 00:11:40 -0600

    Update GitHubBornOn.js

--- src/GitHubBornOn.js
+++ src/GitHubBornOn.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import { API } from 'aws-amplify';
+import React 
+    , {
+        useState,
+        useEffect
+    }
+from 'react';
 
-export const GitHubBornOn = () => {
+export const GitHubBornOn = async () => {
+
+    //Variable to hold github creation date
+    const [createdOn, showCreationDate] = useState("");
+
+    //Function that will call API get/born
+    const fetchGitHubCreationDate = async () => {
+        try {
+            //Try to get the data from the API endpoint /born
+            const data = await API.get('api9bc74a79', '/born');
+            showCreationDate(data.createOn);
+        }
+        catch(err) {
+            console.error(err);
+        }
+    };
+
+    //Call fetchGitHubCreationDate when component loads
+    useEffect(
+        () => {
+            fetchGitHubCreationDate();
+        }
+        ,[]
+    );
     return(
         <h2>
-            my github name goes here - my github created at goes here
+            my github name goes here - my github created at goes here{createdOn}
         </h2>
     );
 };
-
